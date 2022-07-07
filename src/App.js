7d7e4a7669d404f901b8cfb96b45874f6c929658
Mateusz <mateuszzwierzchowski19@gmail.com>
2022-07-07 21:54:21 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ListItem from './ListItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    activities: ["Pet the cat", "Feed fish in aquarium", "Practice some coding"],
+    inputValue: '',
+    checkedActivities: []
+  }
+
+  handleSubmit = (event) => {
+    if(this.state.inputValue) {
+      this.setState( {
+        activities: [...this.state.activities, this.state.inputValue],
+        inputValue: ''
+      })
+    }
+    event.preventDefault();
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+
+  handleClick = (e) => {
+
+    var value = e.target.value;
+
+    this.setState({
+      checkedActivities: [...this.state.checkedActivities, value],
+      activities: this.state.activities.filter(function(activity) {
+        return activity !== value;
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="left-panel">
+          <h1>React planner</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>Activity
+            <input type="text" value={this.state.inputValue} onChange={this.handleInputChange}/>
+            </label>
+            <input type="submit" value="Add activity" />
+          </form>
+          <div className="list">
+            <ul>
+              {
+                this.state.activities.map((item, i) => (
+                  <li key={i}>{item}<button value={item} onClick={this.handleClick}>Done</button></li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+        <div className="right-panel">
+          <h2>Checked activities</h2>
+          <div className="list">
+            <ul>
+                {this.state.checkedActivities.map((item, i) => (
+                  <ListItem key={i} item={item}/>
+                ))
+                }
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
